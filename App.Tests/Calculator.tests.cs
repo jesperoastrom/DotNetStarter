9fe513647907ce29830919fb8ee538e1265c1fda
Jesper Åström <jesper.o.astrom@gmail.com>
2016-11-26 22:25:52 +0100

    Initial commit

---
+++ App.Tests/Calculator.tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace App.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void CalculatorCanAdd()
+        {
+            var calculator = new Calculator();
+            Assert.Equal(2, calculator.Add(1, 1));
+        }
+    }
+}
